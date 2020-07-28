3f83fb07123e4fc997a957f15d27ef9e9915e1d3
Chen-Yu Pao <windperson@gmail.com>
2020-07-28 16:12:36 +0800

    Before using EF Core migrations

---
+++ BingoDb.Migrations/BingoDbContextDesignTimeFactory.cs
@@ -0,0 +1,18 @@
+﻿using GranDen.Game.ApiLib.Bingo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BingoDb.Migrations
+{
+    public class BingoDbContextDesignTimeFactory : IDesignTimeDbContextFactory<BingoGameDbContext>
+    {
+        public BingoGameDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BingoGameDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DemoBingoDb",
+                x => x.MigrationsAssembly(typeof(BingoDbContextDesignTimeFactory).Assembly.GetName().Name));
+
+            return new BingoGameDbContext(optionsBuilder.Options);
+        }
+    }
+}
